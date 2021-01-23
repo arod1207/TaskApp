0a17032f0a41cda6a09576f63bc90bdd17689bd9
Armando Rodriguez <arod1207@me.com>
2021-01-23 14:36:27 -0600

    added delete on serverside

--- Routes/TaskAPI.js
+++ Routes/TaskAPI.js
@@ -30,4 +30,15 @@ Router.get('/tasks', (req, res) => {
         });
 });
 
+Router.delete('/tasks/:id', (req, res) => {
+    const id = req.params.id;
+    Task.findByIdAndDelete(id)
+        .then((results) => {
+            res.status(200).json(results);
+        })
+        .catch((error) => {
+            res.status(500).json(error);
+        });
+});
+
 module.exports = Router;
