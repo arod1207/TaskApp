2edf0c8219ee688309f611ffebf73d5edca5ca06
Armando Rodriguez <arod1207@me.com>
2021-01-22 20:06:23 -0600

    finised task app

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Task App</h1>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Task.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import { useContext, useState } from 'react';
+import { TaskContext } from '../TaskContext';
+
+const Task = () => {
+    const [tasks, setTasks] = useContext(TaskContext);
+
+    const deleteHandler = (id) => {
+        axios.delete(`http://localhost:5000/tasks/${id}`);
+        setTasks(tasks.filter((task) => task.id !== id));
+    };
+
+    const toggleHandler = (id) => {
+        setTasks(
+            tasks.map((task) =>
+                task.id === id ? { ...task, done: !task.done } : task
+            )
+        );
+    };
+
+    return (
+        <div className="task">
+            {tasks.length === 0 ? (
+                <h1>No Task Today</h1>
+            ) : (
+                tasks.map((task) => (
+                    <div
+                        key={task.id}
+                        className={`tasks ${task.done ? 'task__complete' : ''}`}
+                        onDoubleClick={() => toggleHandler(task.id)}
+                    >
+                        <h3>{task.newTask}</h3>
+                        <div className="task__Delete">
+                            <i
+                                className="fas fa-trash-alt fa-lg"
+                                onClick={() => deleteHandler(task.id)}
+                            ></i>
+                        </div>
+                    </div>
+                ))
+            )}
+        </div>
+    );
+};
+
+export default Task;
